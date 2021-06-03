ae85dd1cb0b36d895430576a881803c3d3d22ade
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-06-03 11:28:44 +0300

    complete advanced part

--- index.js
+++
@@ -1,4 +0,0 @@
-const salmon = 15.678;
-const bread = 123.965;
-const tomato = 90.2345;
-console.log(salmon);
---
+++ script.js
@@ -0,0 +1,60 @@
+const salmon = 15.678;
+const bread = 123.965;
+const tomato = 90.2345;
+const maxProductPrice = Math.max(salmon, bread, tomato);
+console.log("Maximal product price = " + maxProductPrice);
+
+const minProductPrice = Math.min(salmon, bread, tomato);
+console.log("Minimal product price = " + minProductPrice);
+
+const sumProducts = salmon + bread + tomato;
+console.log("Products sum = " + sumProducts);
+
+const sumFloor = Math.floor(sumProducts);
+console.log(sumFloor);
+
+const sumRound = Math.round((sumProducts) / 100) * 100;
+console.log(sumRound);
+
+function boolean(sumProducts) {
+  if(sumProducts % 2 === 0) {
+    return "number is odd";
+  } else {
+    return "number is even";
+  }
+};
+
+console.log(boolean(sumProducts));
+
+const averageProductsValue = ((salmon + bread + tomato) / 3).toFixed(2);
+console.log("Average products value = " + averageProductsValue);
+
+const discount = Math.random().toFixed(2) * 100;
+console.log("Discount = " + discount);
+
+const incomeProfit = ((sumProducts / 2) - discount).toFixed(2);
+console.log("Price with discount = " + incomeProfit);
+
+//Advanced
+
+const table = document.getElementById("table-result");
+
+const firstRow = table.insertRow(0);
+
+const names = ["Salmon price", "Bread price", "Tomato price",
+  "Max price", "Min price",
+  "Sum products", "Sum floor", "Sum round", "Boolean",
+  "Average products value", "Discount", "Profit income"
+];
+
+names.forEach(element => firstRow.insertCell(names.indexOf(element)).innerHTML = element)
+
+const secondRow = table.insertRow(1);
+
+const digits = [salmon, bread, tomato,
+  maxProductPrice, minProductPrice, sumProducts, sumFloor, sumRound,
+  boolean(sumProducts), averageProductsValue, discount, incomeProfit
+];
+
+digits.forEach(element => secondRow.insertCell(digits.indexOf(element)).innerHTML = element);
+
